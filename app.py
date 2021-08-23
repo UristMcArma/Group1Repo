8f8041cec7038bf1d5b656ad9a5f7c2d35c7f814
50037331 <89388755+50037331@users.noreply.github.com>
2021-08-23 16:25:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Testing that this thing updates."
+
+group = ["Alex", "Anna", "Ariadna", "Bronagh", "Conor"]
+for name in group:
+   print(name)
